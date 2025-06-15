452ddd7785b750d51a6b17e8ef1683a04cdb0f0b
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-15 09:44:33 +0600

    modify change

--- server/routes/doctorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDoctors, addDoctor } = require('../controllers/doctorController');
-
-router.get('/', getDoctors);      // ?name=Ali&specialization=Cardiology
-router.post('/', addDoctor);      // Add new doctor
-
-module.exports = router;
