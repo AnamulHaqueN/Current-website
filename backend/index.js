cf2579ac650c8e6c02946361e9c125d7fd1e4a92
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-16 09:54:52 +0600

    Changes redirect issues for About, Service, Pricing, Contact pages

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ const app = express();
 const port = 8080;
 
 // MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/hospital', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("MongoDB connected");
-}).catch(err => console.log(err));
+main().then((res)=>{
+    console.log("mongodb is connected successfuly");
+}).catch((err) => {
+    console.log(err);
+});
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/hospital");
+}
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
